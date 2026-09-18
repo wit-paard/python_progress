c9c48d4835174ce6918bfec62554877f937fb407
wit-paard <konyschew.ilja2004@yandex.ru>
2026-09-19 01:56:39 +0700

    first commit

---
+++ d2.py
@@ -0,0 +1,14 @@
+# Error value text
+err = print("n/a")
+
+# Here's an elementary I/O for Python.
+print("Hello!")
+print("Can you tell me who I'm talking to?")
+
+# And there's "scanf" analog for this PL.
+# Also, "sep" is for separator between functions, and "end" shows what symbol should be at the end of every action of function.
+name = input("")
+print('Oh, so you are ', name, ". Nice to meet you!", sep="")
+print('Now I want you to type a number you prefer.')
+integer = int(input(""))
+print(integer, '... okay, I see.')
