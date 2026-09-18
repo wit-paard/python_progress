3f69d3f1b198dfd53c882f648253ec6fe651371e
wit-paard <konyschew.ilja2004@yandex.ru>
2026-09-19 02:30:41 +0700

    BRB next day, needa correct last functions

--- d2.py
+++ d2.py
@@ -1,3 +1,6 @@
+# Not necessary library
+import time
+
 # Error value text
 err = print("n/a")
 
@@ -12,3 +15,28 @@ print('Oh, so you are ', name, ". Nice to meet you!", sep="")
 print('Now I want you to type a number you prefer.')
 integer = int(input(""))
 print(integer, '... okay, I see.')
+time.sleep(3) # kind of delay (needs 'time' library as it seems to)
+print("Since you can use your keyboard, I want you to type two integers for my")
+print("ULTIMATE"); print ("AND"); print("ASTONISHING"); print("mathematical abilities.")
+
+first_num = int(input(""))
+second_num = int(input(""))
+print("So you chose ", first_num, "and ", second_num, ".", sep="")
+time.sleep(2)
+print("And my ONLY right answer is...")
+time.sleep(3)
+print("...")
+time.sleep(1)
+print("...")
+time.sleep(2.2)
+print('"u gay"')
+time.sleep(0.5)
+print("... WAIT WAIT WAIT NO NO NO!!!!!!!!!")
+print("It is:")
+
+try:
+    division = first_num / second_num
+except ZeroDivisionError:
+    print(first_num + second_num, first_num - second_num, first_num * second_num, "YOU DUMB OR SMTH")
+print(first_num + second_num, first_num - second_num, first_num * second_num, first_num / second_num)
+# It's 2:30 AM GMT+7, I wanna sleep... gn xoxo
